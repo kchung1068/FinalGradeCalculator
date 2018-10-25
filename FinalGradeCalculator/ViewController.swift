9338803da7d7f4ab8a9fa3dfbb7645fbe10a4584
Kyle Chung <kchung1068@jh-mba158-04.d214.org>
2018-10-25 14:34:18 -0500

    Initial Commit

---
+++ FinalGradeCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalGradeCalculator
+//
+//  Created by Kyle Chung on 10/25/18.
+//  Copyright © 2018 Kyle Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
